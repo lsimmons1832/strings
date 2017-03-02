94170d7d2b58c0ab151ab07ad38563a456ed76f8
Latasha Simmons <lsimmons1832@gmail.com>
2017-03-01 22:26:02 -0600

    assignment complete

--- strings.js
+++ strings.js
@@ -1,27 +1,41 @@
 var userInput = document.getElementById("textInput");
-var button = document.getElementById("validate"),
+var button = document.getElementById("validate");
 //console.log("userInput", userInput);
 
 //var button = document.getElementById('validate');
 
 
 function reversal(array){
-	var reverseInput = array.split('').reverse().join('');
-	return reverseInput;
+	var reverseString = array.split('').reverse().join('');
+	return reverseString;
+console.log(reverseString);
 }
-console.log(reverseInput)
 
 function alphabits(array) {
-	var string = array.split('').sort().join('');
-	return string;
+	var alphabitsString = array.split('').sort().join('');
+	return alphabitsString;
 }
 
-// function palindrome() {
+function palindrome(array) {
+	var palindromeString = array.split('').reverse().join('');
+	if (array === palindromeString) {
+		return "Your string is a palindrome!";
+	} else {
+		return "It's not a palindrome"
+	}
+}
 
-// }
+button.addEventListener("click", function(){
+	var userInput = document.getElementById('textInput').value;
+	var output = document.getElementById('output');
 
-// var testString = "";
-// reversal(testString);
-// alphabits(testString);
-// palindrome(testString);
-button.addEventListener("click", reversal); //tell me what click event does after comma
+	var reverseInput = reversal(userInput);
+	var alphabitsInput = alphabits(userInput);
+	var palindromeInput = palindrome(userInput);
+
+	if (userInput === '') {
+		output.innerHTML = "Please enter text";
+	} else {
+		output.innerHTML = "Letters reversed: " + reverseInput + "Letters alphabetized: " + alphabitsInput  + palindromeInput;
+	}
+}); //tell me what click event does after comma
